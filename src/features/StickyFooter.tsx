a5ccef5e51b4df65fedfbdea0a1bdb6c6fdc61e8
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2022-04-26 14:58:13 +0300

    ui rework

---
+++ src/features/StickyFooter.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/YaroslavBemov">
+        Yaroslav Bemov
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const StickyFooter = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Copyright />
+      </Container>
+    </Box>
+  )
+}
+
+export default StickyFooter
