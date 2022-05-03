e726f039410e29b9fe3ba137e5dfba32a778cce1
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2022-05-03 15:17:23 +0300

    fix keys

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -8,12 +8,12 @@ import {
 import { IFeedbackData } from "../interfaces";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "md-calc.firebaseapp.com",
   projectId: "md-calc",
   storageBucket: "md-calc.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
